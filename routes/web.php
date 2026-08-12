fe97dda644a83f5d45595b218f31003ea56b3b18
Diego Mesa <mesaospinadiego@gmail.com>
2026-08-11 23:48:04 -0500

    Refactor home route and add about page
    Added an about page route with view data.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name("home.index");
+
+Route::get('/about', function () {
+    $data1 = "About us - Online Store";
+    $data2 = "About us";
+    $description = "This is an about page ...";
+    $author = "Developed by: Your Name";
+    return view('home.about')->with("title", $data1)
+        ->with("subtitle", $data2)
+        ->with("description", $description)
+        ->with("author", $author);
+})->name("home.about");
